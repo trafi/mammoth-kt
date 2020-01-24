862be28c8d25a138525ced006bc72c44fbfcd08b
Justas Medeisis <justas@trafi.com>
2020-01-24 16:19:26 +0200

    Mammoth schema model.

---
+++ src/main/kotlin/com/trafi/mammoth/Schema.kt
@@ -0,0 +1,47 @@
+package com.trafi.mammoth
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Schema(
+    val projectId: String,
+    val versionNumber: Int,
+    val events: List<Event>,
+    val types: List<Type>
+) {
+
+    @Serializable
+    class Event(
+        val name: String,
+        val description: String,
+        val values: List<Value>,
+        val parameters: List<Parameter>
+    ) {
+        companion object {
+            const val eventTypeIdentifier = "EventType"
+        }
+
+        @Serializable
+        class Value(
+            val parameter: Parameter,
+            val stringValue: String?,
+            val integerValue: Int?,
+            val stringEnumValue: String?
+        )
+
+        @Serializable
+        class Parameter(
+            val name: String,
+            val typeName: String,
+            val description: String,
+            val publishName: String
+        )
+    }
+
+    @Serializable
+    class Type(
+        val name: String,
+        val stringEnum: List<String>?
+    )
+
+}
