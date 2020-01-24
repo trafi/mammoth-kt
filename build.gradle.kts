db0aa4a57fe336ead21926ab5632de6f705e4283
Justas Medeisis <justas@trafi.com>
2020-01-24 16:17:20 +0200

    Add kotlinx-serialization dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
+    val kotlinVersion = "1.3.61"
+    kotlin("jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
 }
 
 group = "com.trafi"
@@ -7,10 +9,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
