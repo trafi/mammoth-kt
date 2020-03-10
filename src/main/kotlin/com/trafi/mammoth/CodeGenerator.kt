34ee6e5dd036f8e93e6798a30728c5da80a83831
Justas Medeisis <justas@trafi.com>
2020-03-10 13:00:44 +0200

    Do not insert event tags into the kdoc.

--- src/main/kotlin/com/trafi/mammoth/CodeGenerator.kt
+++ src/main/kotlin/com/trafi/mammoth/CodeGenerator.kt
@@ -61,11 +61,6 @@ object CodeGenerator {
     private fun generateEventFunction(event: Schema.Event): FunSpec {
         return FunSpec.builder(event.nativeFunctionName)
             .addKdoc(event.description)
-            .apply {
-                if (event.tags.isNotEmpty()) {
-                    addKdoc(event.tags.joinToString(prefix = "\n\n", separator = ", ") { "${it.clazz}:${it.name}" })
-                }
-            }
             .returns(rawEvent)
             .addParameters(event.parameters.map { parameter ->
                 ParameterSpec
