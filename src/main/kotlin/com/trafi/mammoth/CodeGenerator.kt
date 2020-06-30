7de293812b325b90be00e07abd6d3dfe8d71acd4
Justas Medeisis <justas@trafi.com>
2020-06-30 11:25:53 +0300

    Generate both business and publish variants of RawEvent. Group the two in a new Analytics.Event.

--- src/main/kotlin/com/trafi/mammoth/CodeGenerator.kt
+++ src/main/kotlin/com/trafi/mammoth/CodeGenerator.kt
@@ -6,11 +6,14 @@ private const val packageName = "com.trafi.analytics"
 private const val enumValuePropertyName = "value"
 private const val schemaVersionPropertyName = "mammothSchemaVersion"
 private const val schemaVersionPublishName = "score"
+private const val schemaVersionBusinessName = "schema_version"
 private const val eventIdPublishName = "achievement_id"
+private const val eventIdBusinessName = "schema_event_id"
 
 object CodeGenerator {
-    private val analytics = ClassName(packageName, "Analytics")
-    private val rawEvent = ClassName(packageName, "RawEvent")
+    private val analyticsObject = ClassName(packageName, "Analytics")
+    private val eventClass = ClassName(packageName, "Analytics", "Event")
+    private val rawEventClass = ClassName(packageName, "RawEvent")
     private val schemaVersion = MemberName(packageName, schemaVersionPropertyName)
 
     fun generateCode(schema: Schema): String {
@@ -61,7 +64,7 @@ object CodeGenerator {
     private fun generateEventFunction(event: Schema.Event): FunSpec {
         return FunSpec.builder(event.nativeFunctionName)
             .addKdoc(event.description)
-            .returns(rawEvent)
+            .returns(eventClass)
             .addParameters(event.parameters.map { parameter ->
                 ParameterSpec
                     .builder(parameter.nativeParameterName, parameter.nativeTypeName)
@@ -70,50 +73,98 @@ object CodeGenerator {
                         when (parameter.name) {
                             Schema.Event.Parameter.screenNameParameterName,
                             Schema.Event.Parameter.previousScreenNameParameterName -> {
-                                defaultValue("%T.screenName", analytics)
+                                defaultValue("%T.screenName", analyticsObject)
                             }
                             Schema.Event.Parameter.modalNameParameterName -> {
-                                defaultValue("%T.modalName", analytics)
+                                defaultValue("%T.modalName", analyticsObject)
                             }
                         }
                     }
                     .build()
             }.sortedBy { it.defaultValue != null })
             .addStatement(
-                "return %T(\n⇥name = %S,\nparameters = %L⇤\n)",
-                rawEvent,
-                event.publishName,
-                CodeBlock.of(
-                    "mapOf(\n⇥%L⇤\n)",
-                    event.publishValues.map {
-                        CodeBlock.of(
-                            "%S to %S",
-                            it.first,
-                            it.second
-                        )
-                    }.plus(event.publishParameterExpressions.map {
-                        CodeBlock.of(
-                            "%S to %L",
-                            it.first,
-                            it.second
-                        )
-                    }).plus(
-                        CodeBlock.of(
-                            "%S to %S",
-                            eventIdPublishName,
-                            event.id
-                        )
-                    ).plus(
-                        CodeBlock.of(
-                            "%S to %M",
-                            schemaVersionPublishName,
-                            schemaVersion
-                        )
-                    ).joinToCode(separator = ",\n")
-                )
+                "return %T(\n⇥business = %L,\npublish = %L⇤\n)",
+                eventClass,
+                generateBusinessEvent(event),
+                generatePublishEvent(event)
             )
             .build()
     }
+
+    private fun generatePublishEvent(event: Schema.Event): CodeBlock {
+        return generateRawEvent(
+            name = event.publishName,
+            parameterCodeBlocks = event.publishValues.map {
+                CodeBlock.of(
+                    "%S to %S",
+                    it.first,
+                    it.second
+                )
+            }.plus(event.publishParameterExpressions.map {
+                CodeBlock.of(
+                    "%S to %L",
+                    it.first,
+                    it.second
+                )
+            }).plus(event.publishMetadataParameters)
+        )
+    }
+
+    private fun generateBusinessEvent(event: Schema.Event): CodeBlock {
+        return generateRawEvent(
+            name = event.name,
+            parameterCodeBlocks = event.businessValues.map {
+                CodeBlock.of(
+                    "%S to %S",
+                    it.first,
+                    it.second
+                )
+            }.plus(event.businessParameterExpressions.map {
+                CodeBlock.of(
+                    "%S to %L",
+                    it.first,
+                    it.second
+                )
+            }).plus(event.businessMetadataParameters)
+        )
+    }
+
+    private fun generateRawEvent(name: String, parameterCodeBlocks: List<CodeBlock>): CodeBlock {
+        return CodeBlock.of("%T(\n⇥name = %S,\nparameters = %L⇤\n)",
+            rawEventClass,
+            name,
+            CodeBlock.of(
+                "mapOf(\n⇥%L⇤\n)",
+                parameterCodeBlocks.joinToCode(separator = ",\n")
+            )
+        )
+    }
+
+    private val Schema.Event.publishMetadataParameters: List<CodeBlock> get() = listOf(
+        CodeBlock.of(
+            "%S to %S",
+            eventIdPublishName,
+            id
+        ),
+        CodeBlock.of(
+            "%S to %M",
+            schemaVersionPublishName,
+            schemaVersion
+        )
+    )
+
+    private val Schema.Event.businessMetadataParameters: List<CodeBlock> get() = listOf(
+        CodeBlock.of(
+            "%S to %S",
+            eventIdBusinessName,
+            id
+        ),
+        CodeBlock.of(
+            "%S to %M",
+            schemaVersionBusinessName,
+            schemaVersion
+        )
+    )
 }
 
 private val Schema.Event.nativeFunctionName: String get() = name.normalized.decapitalize()
@@ -141,12 +192,18 @@ private val Schema.Event.publishValues: List<Pair<String, String>>
         .filterNot { it.parameter.name == Schema.Event.Parameter.eventTypeParameterName }
         .map { it.parameter.publishName to it.publishValue }
 
+private val Schema.Event.businessValues: List<Pair<String, String>>
+    get() = values.map { it.parameter.name to it.publishValue }
+
 private val Schema.Event.Value.publishValue: String
     get() = stringValue ?: integerValue?.toString() ?: stringEnumValue ?: booleanValue?.toString()
     ?: throw IllegalArgumentException("Invalid publish parameter value. Parameter: ${parameter.name}")
 
 private val Schema.Event.publishParameterExpressions: List<Pair<String, String>>
     get() = parameters.map { it.publishName to it.nativeParameterExpression }
+
+private val Schema.Event.businessParameterExpressions: List<Pair<String, String>>
+    get() = parameters.map { it.name to it.nativeParameterExpression }
 
 private val Schema.Event.Parameter.nativeParameterExpression: String
     get() = when (typeName) {
